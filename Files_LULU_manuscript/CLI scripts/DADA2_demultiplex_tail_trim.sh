e9025f6359815f249676ae97e1bf914940380ea6
tobiasgf <tobiasgf@snm.ku.dk>
2017-06-29 18:59:53 +0200

    change to right script

--- Files_LULU_manuscript/CLI scripts/DADA2_demultiplex_tail_trim.sh
+++ Files_LULU_manuscript/CLI scripts/DADA2_demultiplex_tail_trim.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-#### Description: Demultiplexing of fastq reads for processing with DADA2
-
-#### Written by: Tobias Guldberg Frøslev - tobiasgf@bio.ku.dk  on 3-2017
-
 mkdir DADA2_AS
 mkdir DADA2_SS
 
@@ -14,11 +9,19 @@ CUR_PATH=$(pwd)
 
 # Define binaries, temporary files and output files
 CUTADAPT="$(which cutadapt) --discard-untrimmed --minimum-length ${MIN_LENGTH} -e 0"
+CUTADAPT2="$(which cutadapt) -e 0"
 VSEARCH=$(which vsearch)
 C1_FASTQ=$(mktemp)
 C2_FASTQ=$(mktemp)
+TMP_FASTQ=$(mktemp)
 MIN_F=$((${#PRIMER_F}))
 MIN_R=$((${#PRIMER_R}))
+
+REV_PRIMER_F="$(echo $PRIMER_F | rev | tr ATUGCYRKMBDHVN TAACGRYMKVHDBN)"
+REV_PRIMER_R="$(echo $PRIMER_R | rev | tr ATUGCYRKMBDHVN TAACGRYMKVHDBN)"
+
+rev="$(echo $primer | rev | tr ATUGCYRKMBDHVN TAACGRYMKVHDBN)"
+
 
 while read TAG_NAME TAG_SEQ RTAG_SEQ; do
     LOG="DADA2_SS/${TAG_NAME}_R1.log"
@@ -27,7 +30,9 @@ while read TAG_NAME TAG_SEQ RTAG_SEQ; do
     # Trim tags, forward & reverse primers (search normal and antisens)
         cat "${INPUT_R1}" | \
         ${CUTADAPT} -g "${TAG_SEQ}" -e 0 -O "${#TAG_SEQ}" - 2> "${LOG}" | \
-        ${CUTADAPT} -g "${PRIMER_F}" -O "${MIN_F}" - 2>> "${LOG}" > "${FINAL_FASTQ}"
+        ${CUTADAPT} -g "${PRIMER_F}" -O "${MIN_F}" - 2>> "${LOG}" > "${TMP_FASTQ}"
+
+	cat "${TMP_FASTQ}" | ${CUTADAPT2} -a "${REV_PRIMER_R}" - 2>> "${LOG}"  > "${FINAL_FASTQ}"
 
     LOG="DADA2_AS/${TAG_NAME}_R2.log"
     FINAL_FASTQ="DADA2_AS/${TAG_NAME}_R2.fastq"
@@ -35,8 +40,9 @@ while read TAG_NAME TAG_SEQ RTAG_SEQ; do
     # Trim tags, forward & reverse primers (search normal and antisens)
         cat "${INPUT_R2}" | \
         ${CUTADAPT} -g "${TAG_SEQ}" -e 0 -O "${#TAG_SEQ}" - 2> "${LOG}" | \
-        ${CUTADAPT} -g "${PRIMER_F}" -O "${MIN_F}" - 2>> "${LOG}" > "${FINAL_FASTQ}"
+        ${CUTADAPT} -g "${PRIMER_F}" -O "${MIN_F}" - 2>> "${LOG}" > "${TMP_FASTQ}"
 
+        cat "${TMP_FASTQ}" | ${CUTADAPT2} -a "${REV_PRIMER_R}" - 2>> "${LOG}" > "${FINAL_FASTQ}"
 
     LOG="DADA2_SS/${TAG_NAME}_R2.log"
     FINAL_FASTQ="DADA2_SS/${TAG_NAME}_R2.fastq"
@@ -44,7 +50,9 @@ while read TAG_NAME TAG_SEQ RTAG_SEQ; do
     # Trim tags, forward & reverse primers (search normal and antisens)
         cat "${INPUT_R2}" | \
         ${CUTADAPT} -g "${RTAG_SEQ}" -e 0 -O "${#RTAG_SEQ}" - 2>> "${LOG}" | \
-        ${CUTADAPT} -g "${PRIMER_R}" -O "${MIN_R}" - 2>> "${LOG}" > "${FINAL_FASTQ}"
+        ${CUTADAPT} -g "${PRIMER_R}" -O "${MIN_R}" - 2>> "${LOG}" > "${TMP_FASTQ}"
+
+        cat "${TMP_FASTQ}" | ${CUTADAPT2} -a "${REV_PRIMER_F}" - 2>> "${LOG}" > "${FINAL_FASTQ}"
 
     LOG="DADA2_AS/${TAG_NAME}_R1.log"
     FINAL_FASTQ="DADA2_AS/${TAG_NAME}_R1.fastq"
@@ -52,11 +60,13 @@ while read TAG_NAME TAG_SEQ RTAG_SEQ; do
     # Trim tags, forward & reverse primers (search normal and antisens)
         cat "${INPUT_R1}" | \
         ${CUTADAPT} -g "${RTAG_SEQ}" -e 0 -O "${#RTAG_SEQ}" - 2>> "${LOG}" | \
-        ${CUTADAPT} -g "${PRIMER_R}" -O "${MIN_R}" - 2>> "${LOG}" > "${FINAL_FASTQ}"
+        ${CUTADAPT} -g "${PRIMER_R}" -O "${MIN_R}" - 2>> "${LOG}" > "${TMP_FASTQ}"
+
+        cat "${TMP_FASTQ}" | ${CUTADAPT2} -a "${REV_PRIMER_F}" - 2>> "${LOG}" > "${FINAL_FASTQ}"
 
 done < "${TAGS}"
 
 # Clean
-rm -rf "${C1_FASTQ}" "${C2_FASTQ}"
+rm -rf "${C1_FASTQ}" "${C2_FASTQ}" "${TMP_FASTQ}"
 
 done < batchfileDADA2.list
